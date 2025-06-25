37028e62fb06a4d496e161fa9d7c297923ae9965
Anthony James Moran <75617672+doppie-320@users.noreply.github.com>
2025-06-25 18:38:25 +0800

    Revert "Merge branch 'feat/fare-system'"
    This reverts commit 82ac9a4a6859981ef250233a2471a8db8f991c4b, reversing
    changes made to a9fdda9ee04b50dd687ce478cea0f3fc90f52ba3.

--- backend/internal-devkit/multimodal-route-solver/logic/main.js
+++ backend/internal-devkit/multimodal-route-solver/logic/main.js
@@ -26,8 +26,6 @@ $(document).ready(function () {
             [markerPositions.endPos.lat, markerPositions.endPos.lng]
         );
 
-        console.log(calculateFareFromMerged(mergedLegs, loadedRoutes));
-
         // console.log(path);
     }
 
@@ -67,7 +65,7 @@ $(document).ready(function () {
                         }
 
                         //Create a shorter list of coordinates as path, reducing coords to reduce calculations
-                        const TRUNCATION_SEPARATION = 100; //METERS
+                        const TRUNCATION_SEPARATION = 300; //METERS
                         const shortPath = calculateTruncatedPath(fullPath, TRUNCATION_SEPARATION);                        
 
                         //Maps truncated paths to their full conterpart
