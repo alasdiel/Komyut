1252a0f4533b59c166abcc2cc4875b48eafdbbeb
Anthony James Moran <75617672+doppie-320@users.noreply.github.com>
2025-07-06 13:16:01 +0800

    chore: delete existing files in infrastructure/*

--- infrastructure/lib/stateless/constructs/construct.ts
+++
@@ -1,9 +0,0 @@
-import { Construct } from "constructs";
-
-interface SampleConstructProps {}
-
-export class SampleConstruct extends Construct {
-  constructor(scope: Construct, id: string, props: SampleConstructProps) {
-    super(scope, id);
-  }
-}
