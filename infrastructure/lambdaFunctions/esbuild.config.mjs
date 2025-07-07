b62741d9cc23ba8a31cd034137392069629ebe9b
gelooooo-git <kojose2@up.edu.ph>
2025-07-07 16:54:40 +0800

    commiting update to functions, messed up something on other files

--- infrastructure/lambdaFunctions/esbuild.config.mjs
+++ infrastructure/lambdaFunctions/esbuild.config.mjs
@@ -4,9 +4,10 @@ import path from "path";
 build({
   packages: 'external',
   entryPoints: [
-    "./src/login/login.ts",
-    "./src/createUser/createUser.ts",
     "./src/helloWorld/helloWorld.ts",
+    "./src/confirmSignup/confirmSignup.ts",
+    "./src/signup/signup.ts",
+    "./src/signin/signin.ts",
     // add more as needed
   ],
   entryNames: "[dir]/index",
