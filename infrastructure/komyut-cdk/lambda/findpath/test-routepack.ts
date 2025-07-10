198fc1a5ea1be7c5cb3e642052c72d61868282aa
Anthony James Moran <75617672+doppie-320@users.noreply.github.com>
2025-07-10 23:49:08 +0800

    exp: feat/route-calc-lambda

--- infrastructure/komyut-cdk/lambda/findpath/test-routepack.ts
+++ infrastructure/komyut-cdk/lambda/findpath/test-routepack.ts
@@ -1,16 +1,26 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { laodRoutePackBundleFromS3, loadRoutePack, loadRoutePackFromS3, loadRoutePackFromS3Parallel } from "../../helpers/routepackLoader";
 import { RoutePack } from "@shared/types";
+import { findBestPath } from "../../calculation/routesolver";
 
 let cachedRoutePack: RoutePack | null = null;
 
 export const handler: APIGatewayProxyHandler = async (event) => {
     try {
+        //Load path
         if (!cachedRoutePack) {
             console.log("COLD START, loading routepack");
             // cachedRoutePack = await loadRoutePackFromS3Parallel('komyut-routepack-bucket', 'routepack');
             cachedRoutePack = await laodRoutePackBundleFromS3('komyut-routepack-bucket', 'routepack-bundle');
         }
+
+        //Find best path
+        // const { coordinates, path } = await findBestPath()
+
+        //Merge
+
+        //Transfer from nodes->coords
+
 
         const routePack = cachedRoutePack;
 
--- infrastructure/komyut-cdk/lib/komyut-cdk-stack.ts
+++ infrastructure/komyut-cdk/lib/komyut-cdk-stack.ts
@@ -8,54 +8,65 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as path from 'path';
 
 export class KomyutCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here    
+		// The code that defines your stack goes here    
 
-    // 💿 DYNAMODB TABLES
+		// 💿 DYNAMODB TABLES
 
-    // ⭐ LAMBDA FUNCTIONS (use lambda-nodejs NodejsFunction() instead to avoid building to .js)
-    const fnHelloWorld = new lambdaNJS.NodejsFunction(this, 'HelloWorldFunction', {
-      entry: path.join(__dirname, '../lambda/helloworld/helloworld.ts'),
-      runtime: lambda.Runtime.NODEJS_20_X,      
-    });
+		// ⭐ LAMBDA FUNCTIONS (use lambda-nodejs NodejsFunction() instead to avoid building to .js)
+		// ANTHONY'S WORK
+		const fnHelloWorld = new lambdaNJS.NodejsFunction(this, 'HelloWorldFunction', {
+			entry: path.join(__dirname, '../lambda/helloworld/helloworld.ts'),
+			runtime: lambda.Runtime.NODEJS_20_X,
+		});
 
-    const fnTestLoadRoutePack = new lambdaNJS.NodejsFunction(this, 'TestLoadRoutePackFunction', {
-      entry: path.join(__dirname, '../lambda/findpath/test-routepack.ts'),
-      runtime: lambda.Runtime.NODEJS_20_X,
-      timeout: cdk.Duration.seconds(120)
-    });
+		const fnTestLoadRoutePack = new lambdaNJS.NodejsFunction(this, 'TestLoadRoutePackFunction', {
+			entry: path.join(__dirname, '../lambda/findpath/test-routepack.ts'),
+			runtime: lambda.Runtime.NODEJS_20_X,
+			timeout: cdk.Duration.seconds(120)
+		});
 
-    const fnConfirmSignup = new lambdaNJS.NodejsFunction(this, 'ConfirmSignupFunction', {
-      entry: path.join(__dirname, '../lambda/confirmSignup/confirmSignup.ts'),
-      runtime: lambda.Runtime.NODEJS_20_X,      
-    });
+		const fnCalcPlan = new lambdaNJS.NodejsFunction(this, 'CalculatePlanFunction', {
+			entry: path.join(__dirname, '../lambda/findpath/findbestpath.ts'),
+			runtime: lambda.Runtime.NODEJS_20_X,
+			timeout: cdk.Duration.seconds(120)
+		});
 
-    const fnSignin = new lambdaNJS.NodejsFunction(this, 'SigninFunction', {
-      entry: path.join(__dirname, '../lambda/signin/signin.ts'),
-      runtime: lambda.Runtime.NODEJS_20_X,      
-    });
+		// KARLO'S WORK
+		const fnConfirmSignup = new lambdaNJS.NodejsFunction(this, 'ConfirmSignupFunction', {
+			entry: path.join(__dirname, '../lambda/confirmSignup/confirmSignup.ts'),
+			runtime: lambda.Runtime.NODEJS_20_X,
+		});
 
-    const fnSignup = new lambdaNJS.NodejsFunction(this, 'SignupFunction', {
-      entry: path.join(__dirname, '../lambda/signup/signup.ts'),
-      runtime: lambda.Runtime.NODEJS_20_X,
-    });
-    
-    // 🪣 S3 BUCKETS
-    const routePackBucket = new s3.Bucket(this, 'RoutePackBucket', {
-      bucketName: 'komyut-routepack-bucket',
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      publicReadAccess: false,
-    });    
-    routePackBucket.grantRead(fnTestLoadRoutePack);
+		const fnSignin = new lambdaNJS.NodejsFunction(this, 'SigninFunction', {
+			entry: path.join(__dirname, '../lambda/signin/signin.ts'),
+			runtime: lambda.Runtime.NODEJS_20_X,
+		});
 
-    // 🚦 APIGATEWAY DEFINITION
-    const api = new apigw.RestApi(this, 'KomyutRestApi');
-    api.root.addResource('hello-world')
-      .addMethod('GET', new apigw.LambdaIntegration(fnHelloWorld));
-    api.root.addResource('test-routepack')
-      .addMethod('GET', new apigw.LambdaIntegration(fnTestLoadRoutePack));
-  }
+		const fnSignup = new lambdaNJS.NodejsFunction(this, 'SignupFunction', {
+			entry: path.join(__dirname, '../lambda/signup/signup.ts'),
+			runtime: lambda.Runtime.NODEJS_20_X,
+		});
+
+		// 🪣 S3 BUCKETS
+		const routePackBucket = new s3.Bucket(this, 'RoutePackBucket', {
+			bucketName: 'komyut-routepack-bucket',
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+			autoDeleteObjects: true,
+			publicReadAccess: false,
+		});
+		// routePackBucket.grantRead(fnTestLoadRoutePack);
+		routePackBucket.grantRead(fnCalcPlan);
+
+		// 🚦 APIGATEWAY DEFINITION
+		const api = new apigw.RestApi(this, 'KomyutRestApi');
+		api.root.addResource('hello-world')
+			.addMethod('GET', new apigw.LambdaIntegration(fnHelloWorld));
+		// api.root.addResource('test-routepack')
+		//   .addMethod('GET', new apigw.LambdaIntegration(fnTestLoadRoutePack));
+		api.root.addResource('calc-route')
+			.addMethod('POST', new apigw.LambdaIntegration(fnCalcPlan));
+	}
 }
