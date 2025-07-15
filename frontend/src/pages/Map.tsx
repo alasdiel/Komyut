898911fcf8713cef1a30c1f8a91274d1663b241a
alasdiel <alex.albinda9@gmail.com>
2025-07-15 22:39:59 +0800

    fix: ts-expect-error in //

--- frontend/src/pages/Map.tsx
+++ frontend/src/pages/Map.tsx
@@ -6,7 +6,7 @@ export default function Map() {
   return (
     <div className="h-screen w-screen"> 
     {
-    {/* @ts-expect-error leaflet being removed soon*/}
+    // @ts-expect-error leaflet being removed soon
     <MapContainer 
       center={[7.0647, 125.6088]} 
       zoom={13}
@@ -15,8 +15,7 @@ export default function Map() {
       >
     }
     {
-          {/* @ts-expect-error leaflet being removed soon*/}
-          <TileLayer
+          // @ts-expect-error leaflet being removed soon          <TileLayer
             attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           
