5d2e5375921ff8c4e6500b87f7c509a49b07ddb7
alasdiel <alex.albinda9@gmail.com>
2025-07-15 22:34:28 +0800

    fix: add description to ts-expect-error for the linting

--- frontend/src/pages/Map.tsx
+++ frontend/src/pages/Map.tsx
@@ -5,14 +5,14 @@ export default function Map() {
 
   return (
     <div className="h-screen w-screen"> 
-    {/* @ts-expect-error */}
+    {/* @ts-expect-error leaflet being removed soon*/}
     <MapContainer 
       center={[7.0647, 125.6088]} 
       zoom={13}
       className="h-full w-full"
       zoomControl={false}
       >
-          {/* @ts-expect-error */}
+          {/* @ts-expect-error leaflet being removed soon*/}
           <TileLayer
             attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
