40a8550cec79eb5cbef05dfd48e34c75c7902cde
chalorejo <charissecardineslorejo@gmail.com>
2025-07-16 13:50:18 +0800

    feat: added hero section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
+import React from "react";
 import Navbar from "./components/AppNavbar";
 import Footer from "./components/AppFooter";
+import HeroSection from "./components/HeroSection";
 
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
 
-      <main className="flex-grow p-6">
+      {/* <main className="flex-grow p-6">
         <h2 className="text-2xl font-semibold">Hello, world!</h2>
+      </main> */}
+      <main className="flex-grow p-6">
+        <HeroSection />
       </main>
 
       {/*TODO: Our Work Section*/}
@@ -17,6 +22,8 @@ export default function App() {
       {/* Footer */}
       <Footer />
     </div>
+    
+    
   );
 }
 
---
+++ frontend/src/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function HeroSection() {
+  return (
+    <div>
+        <h3>Here is a caption.</h3>
+        <h1>Lorem ipsum dolor sit amet!</h1>
+        <Button>Get Started Now!</Button>
+    </div>
+  );
+}
+export default HeroSection;
