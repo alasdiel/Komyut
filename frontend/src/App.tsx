9c7a2585a61e5d4496a740b1f5029312e1cbdb8e
chalorejo <charissecardineslorejo@gmail.com>
2025-07-16 17:28:08 +0800

    feat: added about section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/AppNavbar";
 import Footer from "./components/AppFooter";
 import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       </main> */}
       <main className="flex-grow p-6">
         <HeroSection />
+        <AboutSection />
       </main>
 
       {/*TODO: Our Work Section*/}
