26cfc95324b62dcd1fa22a64792e51fc3bc509c6
alasdiel <alex.albinda9@gmail.com>
2025-07-16 18:28:03 +0800

    feat!: commented old leaflet components

--- frontend/src/pages/Map.tsx
+++ frontend/src/pages/Map.tsx
@@ -1,26 +1,22 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-nocheck
-import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, ZoomControl } from "react-leaflet"
-
 export default function Map() {
 
   return (
     <div className="h-screen w-screen"> 
-    <MapContainer 
-      center={[7.0647, 125.6088]} 
-      zoom={13}
-      className="h-full w-full"
-      zoomControl={false}
-      >
-          <TileLayer
-            attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          
-            />
-           <ZoomControl position="bottomright" />
-      </MapContainer>
-    </div>
+    {// <MapContainer 
+   //   center={[7.0647, 125.6088]} 
+   //   zoom={13}
+   //   className="h-full w-full"
+   //   zoomControl={false}
+   //   >
+   //       <TileLayer
+   //         attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+   //         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+   //       
+   //         />
+   //        <ZoomControl position="bottomright" />
+   //   </MapContainer>
+    }
+      </div>
   )
 }
 
