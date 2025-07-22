61ce81e374ec441432bceb60cd66a601c4ac1cdd
alasdiel <alex.albinda9@gmail.com>
2025-07-22 20:10:31 +0800

    feat: minimal control panel

--- frontend/src/components/MapComponent.tsx
+++ frontend/src/components/MapComponent.tsx
@@ -1,11 +1,25 @@
 import { useEffect, useRef } from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import { Button } from '@/components/ui/button'
+
 
 const MapComponent = () => {
 	const mapContainer = useRef<HTMLDivElement | null>(null);
 	const map = useRef<maplibregl.Map | null>(null);
 	const stylejson = `https://api.maptiler.com/maps/openstreetmap/style.json?key=${import.meta.env.VITE_MAPTILER_KEY}`;
+
+  //click boilerplate
+  const handleClick = async () => {
+    await fetch("/api/your-endpoint", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ example: "data" }),
+    });
+  };
+
 
 	useEffect(() => {
 		if (!mapContainer.current) return;
@@ -58,7 +72,54 @@ const MapComponent = () => {
 
 	return (
 		<div className="flex flex-row min-h-screen justify-center items-center bg-orange-400" style={{ height: '100vh', width: '100%' }}>
-			<div ref={mapContainer} style={{ height: '95%', width: '95%' }} />
+			<div ref={mapContainer} style={{ height: '100%', width: '100%' }} />
+      <div idName="controls" className="flex items-center justify-center flex-col h-1/3 bg-indigo-950">
+
+        <div className="text-white m-2">
+          <h3 className="font-epilogue">Start Position</h3>
+          <div>
+            <label>
+              Latitude:
+              <input className="bg-white text-black"
+                             />
+            </label>
+            <br />
+            <label>
+              Longitude:
+              <input className="bg-white text-black"
+                
+              />
+            </label>
+          </div>
+        </div>
+
+        <div className="text-white m-2">
+          <h3 className="font-epilogue">End Position</h3>
+          <div>
+            <label>
+              Latitude:
+              <input className="bg-white text-black"
+                
+              />
+            </label>
+            <br />
+            <label>
+              Longitude:
+              <input className="bg-white text-black"
+                
+              />
+            </label>
+          </div>
+        </div>
+        <Button className="py-4 m-2" onClick={handleClick}> 
+          Calc 
+        </Button> 
+          
+        {// ends here
+        }
+
+      </div>
+
 		</div>
 	);
 };
