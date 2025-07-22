546749ede8ac93700642207cccfdfe7587c30f7e
Anthony James Moran <75617672+doppie-320@users.noreply.github.com>
2025-07-22 19:30:38 +0800

    test: write polyline in maplibre

--- frontend/src/components/MapComponent.tsx
+++ frontend/src/components/MapComponent.tsx
@@ -3,39 +3,64 @@ import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
 const MapComponent = () => {
-  const mapContainer = useRef<HTMLDivElement | null>(null);
-  const map = useRef<maplibregl.Map | null>(null);
-  const stylejson = `https://api.maptiler.com/maps/openstreetmap/style.json?key=${import.meta.env.VITE_MAPTILER_KEY}`;
+	const mapContainer = useRef<HTMLDivElement | null>(null);
+	const map = useRef<maplibregl.Map | null>(null);
+	const stylejson = `https://api.maptiler.com/maps/openstreetmap/style.json?key=${import.meta.env.VITE_MAPTILER_KEY}`;
 
-  useEffect(() => {
-    if (!mapContainer.current) return;
+	useEffect(() => {
+		if (!mapContainer.current) return;
 
-    map.current = new maplibregl.Map({
-      container: mapContainer.current,
-      style: stylejson,
-      center: [125.6088, 7.15],
-      zoom: 10,
-      minZoom: 8,
-      maxZoom: 14,
-      maxBounds: 
-      [
+		map.current = new maplibregl.Map({
+			container: mapContainer.current,
+			style: stylejson,
+			center: [125.6088, 7.15],
+			zoom: 10,
+			minZoom: 8,
+			maxZoom: 14,
+			maxBounds:
+				[
 
-      [125.204904, 6.795854], 
-      [125.892734, 7.516401], 
+					[125.204904, 6.795854],
+					[125.892734, 7.516401],
+				]
+		});
 
-      ]
-    });
+		const mapGl = map.current;
+		mapGl.on('load', () => {			
+			//Add route data
+			mapGl.addSource('route', {
+				type: 'geojson',
+				data: {
+					type: 'Feature',
+					properties: {},
+					geometry: {
+						type: 'LineString',
+						coordinates: []
+					}
+				}
+			});			
+			//Draw line from route data
+			mapGl.addLayer({
+				id: 'route',
+				type: 'line',
+				source: 'route',
+				paint: {
+					'line-color': 'green',
+					'line-width': 8
+				}		
+			});
+		});
 
-    return () => {
-      map.current?.remove();
-    };
-  }, []);
+		return () => {
+			map.current?.remove();
+		};
+	}, []);
 
-  return (
-    <div className="flex flex-row min-h-screen justify-center items-center bg-orange-400" style={{ height: '100vh', width: '100%' }}>
-      <div ref={mapContainer} style={{ height: '95%', width: '95%' }} />
-    </div>
-  );
+	return (
+		<div className="flex flex-row min-h-screen justify-center items-center bg-orange-400" style={{ height: '100vh', width: '100%' }}>
+			<div ref={mapContainer} style={{ height: '95%', width: '95%' }} />
+		</div>
+	);
 };
 
 export default MapComponent;
