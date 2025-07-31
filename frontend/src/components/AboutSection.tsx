ecb2ce2ba93932fabee1afa30c522399969a7fbf
chalorejo <charissecardineslorejo@gmail.com>
2025-07-31 16:05:44 +0800

    feat: added contents to about section

--- frontend/src/components/AboutSection.tsx
+++ frontend/src/components/AboutSection.tsx
@@ -2,15 +2,18 @@ function AboutSection() {
   return (
     <section className="px-4 md:px-8 py-16 bg-white">
       <div className="max-w-7xl mx-auto md:text-left">
-        <h2 className="text-komyut-green font-extrabold uppercase text-lg tracking-wider mb-4">
+        <h2 className="font-epilogue text-komyut-green font-extrabold uppercase text-lg tracking-wider mb-4">
           About Us
         </h2>
-        <p className="text-komyut-grey md:text-lg leading-relaxed">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-          et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        <p className="font-epilogue tracking-wider text-komyut-grey md:text-lg leading-relaxed">
+          Komyut is a Davao-based initiative that helps commuters navigate the city’s jeepney system with
+          ease and confidence. We’re building a platform that provides clear, accurate fare estimates and
+          route options, so whether you’re a student heading to class or a worker commuting daily, <strong>you’ll
+          know exactly how much to pay and which jeepney to ride.</strong>
+          <br /><br />
+          Our mission is to solve fare confusion, promote commuter awareness, and support a more organized
+          public transport experience. No more guessing. No more overpaying. With Komyut, Davao jeepney
+          commuting becomes simple, smart, and stress-free.
         </p>
       </div>
     </section>
