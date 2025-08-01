c2272d35df6920d5665ac52e55810d992970c5cd
alasdiel <alex.albinda9@gmail.com>
2025-08-01 19:11:13 +0800

    fix: lint any type fix

--- frontend/src/components/map/useMapStore.ts
+++ frontend/src/components/map/useMapStore.ts
@@ -1,12 +1,13 @@
 import { create } from 'zustand';
 import { Map } from 'maplibre-gl';
+import type { MapController } from "@maptiler/geocoding-control/types";
 
 type MapStore = {
   map: Map | null;
   setMap: (map: Map) => void;
 
-  mapController: any;
-  setMapController: (controller: any) => void;
+  mapController: MapController | null;
+  setMapController: (controller: MapController) => void;
 };
 
 export const useMapStore = create<MapStore>((set) => ({
